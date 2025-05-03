4825489247413929243dd63266a7860bb96f8e68
bexruz <bexruzasqarov08@gmail.com>
2025-05-04 01:47:03 +0500

    add startapp and models.py is completed

---
+++ core/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class Major(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Program(models.Model):
+    FORMAT_CHOICES = [
+        ('online', 'Online'),
+        ('offline', 'Offline'),
+        ('hybrid', 'Hybrid'),
+    ]
+
+    GENDER_CHOICES = [
+        ('male', 'Male🙋🏻‍♂️'),
+        ('female', 'Female🙋🏻‍♀️'),
+        ('any', 'Any🙋🏻‍♂️🙋🏻‍♀️'),
+
+    ]
+
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    full_info = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    deadline = models.DateField()
+    link = models.URLField()
+    country = models.CharField(max_length=100)
+    format = models.CharField(max_length=50, choices=FORMAT_CHOICES)
+    photo = models.ImageField(upload_to='program_photos/', null=True, blank=True)
+    type = models.CharField(max_length=100)
+    funding = models.CharField(max_length=100)
+    start_age = models.PositiveIntegerField()
+    end_age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    major = models.ManyToManyField('Major', blank=True)
+
+    def __str__(self):
+        return self.title
