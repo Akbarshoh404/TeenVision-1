73a2ef6334f1172e902e8288237f993950dc3141
bexruz <bexruzasqarov08@gmail.com>
2025-05-04 01:47:53 +0500

    custom User model

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from core.models import Program
+
+
+class User(AbstractUser):
+    GENDER_CHOICES = [
+        ('male', 'Male🙋🏻‍♂️'),
+        ('female', 'Female🙋🏻‍♀️'),
+    ]
+
+    username = None  # Username o‘chiriladi, email orqali login
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, )
+    country = models.CharField(max_length=100, default='Uzb')
+    liked_programs = models.ManyToManyField(Program, blank=True, related_name='liked_by_users')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # ismingiz, familiyangiz talab qilinadi
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
