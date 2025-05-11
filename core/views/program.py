500471370ddffbece73ee1b440538650e8fe471b
bexruz <bexruzasqarov08@gmail.com>
2025-05-12 01:14:57 +0500

    type: tutorials, programs

--- core/views/program.py
+++ core/views/program.py
@@ -19,6 +19,27 @@ class ProgramViewSet(viewsets.ModelViewSet):
     filterset_class = ProgramFilter
     search_fields = ['title', 'desc']
 
+    @action(detail=False, methods=['get'])
+    def tutorials(self, request):
+        tutorials = self.queryset.filter(type='tutorial')
+
+        page = self.paginate_queryset(tutorials)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(tutorials, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def programs(self, request):
+        programs = self.queryset.exclude(type='tutorial')
+        page = self.paginate_queryset(programs)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(programs, many=True)
+        return Response(serializer.data)
+
     @action(detail=True, methods=['post'], permission_classes=[IsAuthenticatedOrReadOnly])
     def like(self, request, pk=None):
         program = self.get_object()
