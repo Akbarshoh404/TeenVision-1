c1024a74e9931491f51a268bddfaea93a4baed49
bexruz <bexruzasqarov08@gmail.com>
2025-05-17 18:54:35 +0500

    permissions updated

--- core/permissions.py
+++ core/permissions.py
@@ -1,6 +1,18 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminOrSuperuser(BasePermission):
+class IsSuperAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user and (request.user.is_staff or request.user.is_superuser)
+        return request.user and request.user.is_superuser
+
+
+class IsAdminUserOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
