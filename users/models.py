c1962f3de92a8919008dede55657ff3c2bfaa4e6
bexruz <bexruzasqarov08@gmail.com>
2025-05-19 16:45:01 +0500

    choice countries

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin, Group, Permission
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_countries.fields import CountryField
+
 
 from .managers import UserManager
 from core.models import Program
@@ -21,14 +23,14 @@ class User(AbstractUser):
 
     age = models.PositiveIntegerField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
-    country = models.CharField(max_length=100, default='Uzb', null=True, blank=True)
+    country = CountryField(default='UZ', blank=True)
     liked_programs = models.ManyToManyField(Program, blank=True, related_name='liked_by_users')
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []  # ismingiz, familiyangiz talab qilinadi
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
