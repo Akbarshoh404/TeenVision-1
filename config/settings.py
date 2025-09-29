4b101b31e647588fe9a8992131db3ea8007b1902
Akbarshoh <ismatov2009akb@gmail.com>
2025-09-29 23:53:25 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -33,11 +33,11 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://teen-vision.vercel.app/",
-    "https://teen-vision.netlify.app/",
+    "https://teen-vision.vercel.app",
+    "https://teen-vision.netlify.app",
     "https://teenvision.uz",
     "https://teen-vision.uz",
-    "https://teenvision.onrender.com/"
+    "https://teenvision.onrender.com"
 ]
 
 
